ce11f805ba9be7c2076ed79b36f5cd8698243c10
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-07-29 08:30:09 +0200

    fix: add first part of code's structure

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -1,8 +1,26 @@
 //TODO: to make this component
+
+export function GridPhotoBlock() {
+    return(
+        <div className="border border-black rounded-2xl px-1 py-2">
+
+        </div>
+    )
+}
+
 export default function GallerySect() {
     return(
         <section className="">
-
+            <article className="flex justify-around items-center border-4 rounded-2xl border-black">
+                //TODO: button-arrow
+                <div className="border-2 border-black">
+                    
+                </div>
+                //TODO: button-arrow
+            </article>
+            <article className="grid grid-cols-3 h-auto w-auto">
+                {}
+            </article>
         </section>
     )
 }
