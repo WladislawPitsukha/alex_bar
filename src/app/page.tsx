a95c48f3928bc21ecfd26a71750991feb23328ac
Wladislaw Pitsukha <70092332+WladislawPitsukha@users.noreply.github.com>
2025-08-05 09:44:36 +0200

    fix: change mame of component for connection with the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import EventSect from "@/components/EventSection";
 import Footer from "@/components/footer";
 import GallerySect from "@/components/GallerySection";
-import HeroSection from "@/components/HeroSection";
+import HeroSect from "@/components/HeroSection";
 import MapSect from "@/components/MapSection";
 import NavBar from "@/components/navbar";
 
@@ -12,7 +12,7 @@ export default function Home() {
       <>
         <NavBar />
         <main className="flex flex-col items-center">
-          <HeroSection />
+          <HeroSect />
           <GallerySect />
           <EventSect />
           <MapSect />
