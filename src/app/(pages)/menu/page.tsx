b60e72f0abbdbbf7c59470e84568d8451cb1663c
Wladislaw Pitsukha <70092332+WladislawPitsukha@users.noreply.github.com>
2026-04-22 22:41:17 +0200

    fix: add chnages for files, write additional code for the main file

--- src/app/(pages)/menu/page.tsx
+++ src/app/(pages)/menu/page.tsx
@@ -1,1 +1,21 @@
 //TODO: make the page with full-component's structure of menu's page
+
+import AsideMenu from "@/components/AsideMenu";
+import Footer from "@/components/footer";
+import NavBar from "@/components/navbar";
+import SectionMenu from "@/components/SectionMenu";
+
+export default function Home() {
+    return(
+        <>
+            <NavBar />
+            <main className="flex flex-row items-center">
+                <AsideMenu />
+                <div className="">
+                    <SectionMenu />
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
